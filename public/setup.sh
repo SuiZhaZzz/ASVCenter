fa07f37dd3e0f48ad9e67b99fd51143366fdb166
weissle <2016301500298@whu.edu.cn>
2022-06-20 23:20:12 +0800

    Uncomment the wget sentense.

--- public/setup.sh
+++ public/setup.sh
@@ -1,6 +1,6 @@
 if [ ! -d "./cesium" ] 
 then
-# wget https://github.com/CesiumGS/cesium/releases/download/1.94.3/Cesium-1.94.3.zip
+wget https://github.com/CesiumGS/cesium/releases/download/1.94.3/Cesium-1.94.3.zip
 unzip Cesium-1.94.3.zip -d cesium
 rm ./Cesium-1.94.3.zip
 fi
