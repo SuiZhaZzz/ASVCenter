c794c947de8cca22e207bc7e7a3d638b26ed3311
weissle <2016301500298@whu.edu.cn>
2022-07-01 00:07:57 +0800

    replace background picture

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center bg-grey-10 text-white">
     <!-- <h1>视频监控</h1> -->
-    <q-img src="/Home.jpg" :ratio="16 / 9"></q-img>
+    <q-img src="/home-bg.png" :ratio="16 / 9"></q-img>
   </q-page>
 </template>
 
